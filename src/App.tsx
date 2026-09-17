51e6ca29e2f1ba1482a271990211ea63a0935bdb
Tilihub <149177673+Tilihub@users.noreply.github.com>
2026-09-17 12:27:06 +0000

    Added an input and useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
-export function App() {
-  return <h1>hello again</h1>;
+import { useState } from "react";
+
+function App() {
+  const [weight, setWeight] = useState("");
+
+  return (
+    <>
+      <h1>hello again</h1>
+      <input
+        value={weight}
+        onChange={(event) => setWeight(event.target.value)}
+      />
+    </>
+  );
 }
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import App from "./App";
 
 const element = document.getElementById("root");
 
 if (element === null) {
   throw new Error("element is null");
 }
-createRoot(element).render(App());
+createRoot(element).render(<App />);
